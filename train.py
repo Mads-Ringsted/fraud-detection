87163a321f5fb1ec54df4194e786cadf039859c8
Mads Ringsted <mads@Madss-MacBook-Air.local>
2024-11-21 11:18:48 +0100

    add scoring of clusterings

--- train.py
+++ train.py
@@ -7,7 +7,7 @@ from model import AutoEncoder
 from torch.utils.data import DataLoader, TensorDataset
 import matplotlib.pyplot as plt
 
-EPOCHS = 100
+EPOCHS = 250
 BATCH_SIZE = 256
 LEARNING_RATE = 1e-3
 
@@ -22,18 +22,22 @@ def load_data():
 
 X, y = load_data()
 
-X_non_fraud = X[y == 0].sample(2000)
+X_non_fraud = X[y == 0].iloc[:2000]#.sample(5000)
 X_fraud = X[y == 1]
+
 
 scaler = MinMaxScaler()
 X = scaler.fit_transform(X_non_fraud)
+#X = X_non_fraud.values
 
 model = AutoEncoder()
 loss_fn = torch.nn.MSELoss()
 optimizer = torch.optim.Adam(model.parameters(), lr=LEARNING_RATE)
 
-X_train = torch.tensor(X[:1600], dtype=torch.float32)
-X_val = torch.tensor(X[1600:], dtype=torch.float32)
+rows, cols = X.shape
+rows = int(rows*0.8)
+X_train = torch.tensor(X[:rows], dtype=torch.float32)
+X_val = torch.tensor(X[rows:], dtype=torch.float32)
 
 train_dataset = TensorDataset(X_train, X_train)
 val_dataset = TensorDataset(X_val, X_val)
@@ -89,7 +93,7 @@ torch.save(model.state_dict(), 'model.pth')
 
 # Create a plot of the latent space representation of both the non-fraud and fraud data
 model.eval()
-X = torch.tensor(scaler.transform(X_fraud), dtype=torch.float32)
+X = torch.tensor(scaler.transform(X_fraud.iloc[:2000]), dtype=torch.float32)
 with torch.no_grad():
     latent_space = model.encoder(X).numpy()
     plt.scatter(latent_space[:, 0], latent_space[:, 1], c='r', label='Fraud')
