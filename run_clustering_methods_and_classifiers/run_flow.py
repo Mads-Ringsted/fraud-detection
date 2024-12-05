407ea4653cbede8ba88445a97e05526b1dae8864
philoerdal <96106694+philoerdal@users.noreply.github.com>
2024-12-05 20:24:40 +0100

    v2

--- run_clustering_methods_and_classifiers/run_flow.py
+++ run_clustering_methods_and_classifiers/run_flow.py
@@ -2,12 +2,15 @@ import numpy as np
 import pandas as pd
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
-
+from run_clustering_methods_and_classifiers.LouvainAlgorithm import louvain_algorithm
+from run_clustering_methods_and_classifiers.kmeans import kmeans_algorithm
 
 def main():
     X_train, X_test, y_train, y_test = prepare_data()
 
-
+def run_clustering_methods(X_train, X_test):
+    louvain_algorithm(X_train, X_test)
+    kmeans_algorithm(X_train, X_test)
 
 def prepare_data():
     df = pd.read_csv('creditcard.csv')
